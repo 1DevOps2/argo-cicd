6992fa9f4f001742f80b0cf5e2e105569143f232
1DevOps2 <anaskhandevops@gmail.com>
2023-02-20 18:07:34 +0500

    v0.1

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var city string = "Madrid"
+
+	if city == "Madrid" {
+		fmt.Println("esta ciudad pertenece a españa")
+	} else {
+		fmt.Println("esta ciudad no pertenece a españa")
+	}
+}
